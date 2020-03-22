88bf817474c3c06040d01bf66a13b072628e3a91
Rafael Klock <rklock@segware.com.br>
2020-03-22 01:57:39 -0300

    first commit

---
+++ allowip.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import boto3 
+import sys
+
+# by Rafael Klock
+
+ec2 = boto3.client('ec2')
+security_group_id = 'sg-080ce2a7c35e04bca'
+security_group_name = 'PortKnockerSegware'
+
+ip_allow = sys.argv[1]
+
+retorno = ec2.authorize_security_group_ingress(
+        GroupId=security_group_id,
+        IpPermissions=[
+            {'IpProtocol': '-1',
+             'IpRanges': [
+		{'CidrIp': ip_allow,
+		 'Description': 'Allow PortKnock - Segware'
+                }]
+	    }
+        ])
+
+
+print retorno
---
+++ cleanIpSecurityGroupAws.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import boto3 
+import sys
+
+# by Rafael Klock
+
+ec2 = boto3.client('ec2')
+security_group_id = 'sg-080ce2a7c35e04bca'
+security_group_name = 'PortKnockerSegware'
+
+ip_revoke = sys.argv[1]
+
+retorno = ec2.revoke_security_group_ingress(
+        GroupId=security_group_id,
+        IpPermissions=[
+	    {'IpProtocol': '-1',
+             'IpRanges': [{'CidrIp': ip_revoke}]
+	    }
+        ]
+)
+
+#print retorno
